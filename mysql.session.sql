20767a8e293d9e7e67effa512ad6e088ac5dcd6b
Faraifarrell <faraifarouk@gmail.com>
2024-06-27 12:01:01 +0200

    addn

---
+++ mysql.session.sql
@@ -0,0 +1,12 @@
+-- get the list of departments   
+select * from departments;
+
+-- Retrieve the department ID and name for all departments
+select dept_no,dept_name from departments;
+
+
+-- Retrieve the department ID and name for a specific department (e.g., 5)
+select dept_no,dept_name from departments where dept_no = 5;
+
+
+
